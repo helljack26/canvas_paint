92ff454270bc3c0477d19e8549305bc2a7fd2298
helljack26 <helljack22@gmail.com>
2021-09-27 21:44:21 +0300

    Add input, label, style
    Add input for brush color, background color. Label for it.
    Style for page

--- script/main.js
+++ script/main.js
@@ -1,22 +1,54 @@
+// Set width and height for canvas
+const container = document.getElementById('container')
+let h = container.clientHeight;
+let w = container.clientWidth;
 const canvas = document.getElementById('canvas')
+canvas.setAttribute('width', `${w}`)
+canvas.setAttribute('height', `${h}`)
 
+// Render thickness value 
+const thick = document.getElementById('thickness');
+const thickRender = () => {
+    const thicknessOut = document.getElementById('thick-value')
+    const thicknessValue = document.getElementById('thickness').value;
+    thicknessOut.innerText = thicknessValue;
+}
+thick.onchange = () => {
+    thickRender()
+}
+thickRender();
+
+
+// Canvas 
 const ctx = canvas.getContext('2d')
 
-
-// ctx.lineTo(370,15)
-// ctx.lineTo(320,250)
-// ctx.lineTo(30,10)
-// ctx.stroke()
+// Thickness
 let thickness
 document.getElementById('thickness').addEventListener('change', e => {
     thickness = e.target.value
 })
+// Brush color
+let brushColor;
+document.getElementById('colorBr').addEventListener('change', e => {
+    brushColor = e.target.value;
+});
+// Background color
+let backgroundColor;
+document.getElementById('colorBg').addEventListener('change', e => {
+    backgroundColor = e.target.value;
+    document.getElementById('canvas').style.backgroundColor = backgroundColor;
+});
 
+// Drawing function
 const drawingFn = function (moveEvent) {
-    console.dir(moveEvent)
     ctx.lineTo(moveEvent.offsetX, moveEvent.offsetY)
-    ctx.lineWidth = thickness*0.5
-    ctx.lineJoin = 'round'
+    ctx.lineWidth = document.getElementById('thickness').value;
+    ctx.lineWidth = thickness * 0.5;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    // Brush color
+    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = brushColor;
     ctx.stroke()
 }
 canvas.addEventListener('mousedown', e => {
@@ -24,7 +56,7 @@ canvas.addEventListener('mousedown', e => {
     ctx.beginPath()
     let x = e.offsetX
     let y = e.offsetY
-    ctx.moveTo(x,y)
+    ctx.moveTo(x, y)
 
     ctx.stroke()
     canvas.addEventListener('mousemove', drawingFn)
